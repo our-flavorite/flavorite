892ccd0dfab9c02fe8c9afb4107587bcdb38246c
이승은 <tmddms425@naver.com>
2023-07-15 21:13:05 +0900

    Test

--- frontend/src/components/TestComponent/TestComponent.tsx
+++ frontend/src/components/TestComponent/TestComponent.tsx
@@ -2,7 +2,10 @@ import s from './testComponent.module.scss'
 import {cn} from "utils/commonUtils";
 
 const TestComponent = () => {
-  return <div className={cn(s.test_component)}>test component</div>
+  return <div className={cn(s.test_component)}>
+    test component
+    <div>bug!!!</div>
+  </div>
 }
 
 export default TestComponent

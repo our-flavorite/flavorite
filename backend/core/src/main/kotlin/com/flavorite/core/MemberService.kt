464f4cb4e60fb378a979f96743bf6c3391ffdea6
Jangwook Ryu <rxjw95@gmail.com>
2023-08-12 18:05:23 +0900

    chore: 문구 변경

--- backend/core/src/main/kotlin/com/flavorite/core/MemberService.kt
+++ backend/core/src/main/kotlin/com/flavorite/core/MemberService.kt
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Service
 @Service
 class MemberService {
 
-    fun hello(): String = "return corat hello!!!123"
+    fun hello(): String = "return corat hello!!!"
 }

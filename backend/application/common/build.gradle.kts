479dc71ff2dd4eff0c73ce45d140613afba910e6
Jangwook Ryu <rxjw95@gmail.com>
2023-08-16 01:13:01 +0900

    fix: 모듈 분리 및 객체 격리로 불필요한 security 의존성 제거

--- backend/application/common/build.gradle.kts
+++ backend/application/common/build.gradle.kts
@@ -2,7 +2,6 @@ dependencies {
     implementation(project(":core"))
 
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")

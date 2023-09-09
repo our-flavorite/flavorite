46744f23f50610f3bac4e81f109e37faa69a1751
이승은 <tmddms425@naver.com>
2023-09-09 18:37:17 +0900

    Add test axios

--- frontend/src/components/TestComponent/TestComponent.tsx
+++ frontend/src/components/TestComponent/TestComponent.tsx
@@ -1,9 +1,19 @@
 import s from './testComponent.module.scss'
 import {cn} from "utils/commonUtils";
+import axios from "axios";
+import {useState} from "react";
 
 const TestComponent = () => {
+  const [result, setResult] = useState<string | undefined>(undefined)
+  const handleClick= () => {
+    axios.get('localhost:8080/v1/api/ping').then(result => {
+      setResult(result.data)
+    })
+  }
+  
   return <div className={cn(s.test_component)}>
-    gagㅎㅁㅎ
+    <button onClick={handleClick}>버튼</button>
+    <div>fetch 결과: ${result}</div>
   </div>
 }
 

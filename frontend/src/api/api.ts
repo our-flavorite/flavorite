e48d29b1cf272e99b612e094d7b5c5fc69791440
이승은 <tmddms425@naver.com>
2023-12-09 17:25:20 +0900

    Add APIClass

---
+++ frontend/src/api/api.ts
@@ -0,0 +1,69 @@
+import {
+  APIRequestParam,
+  APIRequestParamWithMethod,
+  DEFAULT_TIMEOUT,
+  MutationResponse,
+  QueryResponse
+} from "api/api.types";
+import {API_BASE_PATH} from "api/api.types";
+import {ObjectUtils} from "utils/objectUtils";
+import {QueryUtils} from "utils/queryUtils";
+
+class APIClass {
+  private controller: AbortController
+  
+  constructor() {
+    this.controller = new AbortController()
+  }
+  
+  get<T, R = QueryResponse<T>>(url: string, { params = {}, config = {}}: APIRequestParam): Promise<R | undefined> {
+    const queryUrl = ObjectUtils.isNotEmpty(params) ? `${url}?${QueryUtils.stringify(params)}` : url
+    return this.fetch<R>(queryUrl, {config: {...config, method: 'GET'}})
+  }
+  
+  post<T, R = MutationResponse<T>>(url: string, { params = {}, config = {}}: APIRequestParam): Promise<R | undefined>{
+    return this.fetch<R>(url, {params, config: {...config, method: 'POST'}})
+  }
+  
+  put<T, R = MutationResponse<T>>(url: string, { params = {}, config = {}}: APIRequestParam): Promise<R | undefined>{
+    return this.fetch<R>(url, {params, config: {...config, method: 'PUT'}})
+  }
+  
+  delete<T, R = MutationResponse<T>>(url: string, { params = {}, config = {}}: APIRequestParam): Promise<R | undefined>{
+    return this.fetch<R>(url, {params, config: {...config, method: 'DELETE'}})
+  }
+  
+  private async fetch<R>(url: string, {params, config}: APIRequestParamWithMethod): Promise<R | undefined> {
+    const {timeout= DEFAULT_TIMEOUT, headers = {}} = config
+    const timeoutTimer = setTimeout(() => this.controller.abort, timeout)
+    const fetchUrl = `${API_BASE_PATH}${url}`
+    try {
+      const requestInit: RequestInit = {
+        credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json',
+          ...headers,
+        },
+        method: config.method,
+        signal: this.controller.signal,
+        ...(ObjectUtils.isNotEmpty(params) && { body: JSON.stringify(params) }),
+      }
+      const result = await fetch(fetchUrl, requestInit).then((res) => res.json())
+      const { error, data } = result ?? {}
+      
+      if(error){
+        throw error
+      }
+      // 타입 관련 에러가 많은 경우 zod도입 검토
+      return data
+    } catch (e){
+      // todo - logger setting
+      console.log('error: ', e)
+      throw e
+    } finally {
+      clearTimeout(timeoutTimer)
+    }
+  }
+}
+
+export const API = new APIClass()
--- frontend/src/api/api.types.ts
+++ frontend/src/api/api.types.ts
@@ -1,1 +1,21 @@
 export const API_BASE_PATH = 'http://localhost'
+export const DEFAULT_TIMEOUT = 10000
+
+export interface APIRequestParam {
+  params?: {[key in string]: any}
+  config?: APIRequestConfig
+}
+
+export interface APIRequestParamWithMethod {
+  params?: {[key in string]: any}
+  config?: APIRequestConfig & { method: string }
+}
+
+interface APIRequestConfig {
+  baseUrl?: string
+  headers?: HeadersInit
+  timeout?: number
+}
+
+export type QueryResponse<T = any> = { data: T | undefined }
+export type MutationResponse<T = any> = { data: T | undefined; isSuccess: boolean }
